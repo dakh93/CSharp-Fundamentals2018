9d16e6483cc8e2fe3471cfbeec40daa9d19ea0c0
dakh93 <dakh93>
2018-01-24 22:28:20 +0200

    Extra task added

---
+++ 01.StackAndQueues/07.TrafficLight/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.TrafficLight
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int carsPerGreen = int.Parse(Console.ReadLine());
+            var queueOfCars = new Queue<string>();
+            int totalPassedCars = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+
+                if (input == "end")
+                {
+                    Console.WriteLine($"{totalPassedCars} cars passed the crossroads.");
+                    return;
+                }
+                else if (input == "green")
+                {
+                    int carsToPass = 0;
+
+                    if (carsPerGreen > queueOfCars.Count)
+                    {
+                        carsToPass = queueOfCars.Count;
+                    }
+                    else
+                    {
+                        carsToPass = carsPerGreen;
+                    }
+                    for (int i = 0; i < carsToPass; i++)
+                    {
+                        Console.WriteLine($"{queueOfCars.Dequeue()} passed!");
+                        totalPassedCars++;
+                        
+                    }
+                }
+                else
+                {
+                    queueOfCars.Enqueue(input);
+                }
+
+            }
+        }
+    }
+}
